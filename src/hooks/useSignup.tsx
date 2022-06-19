126f979cfda91f1e4636eeb3af49eb5f9aa3ce1f
dod <dod@mail.com>
2022-06-19 14:23:36 +0530

    Create Firestore Record for User

--- src/hooks/useSignup.tsx
+++ src/hooks/useSignup.tsx
@@ -1,9 +1,10 @@
 import {useEffect, useState} from "react";
-import {projectAuth, projectStorage} from "../firebase/config";
+import {projectAuth, projectFirestore, projectStorage} from "../firebase/config";
 import {useAuthContext} from "./useAuthContext";
 
 type FireBaseSignupProps = {
-    email:string, password:string, displayName:string, thumbnail:any
+    email:string, password:string,
+    displayName:string, thumbnail:any
 }
 
 export const useSignup = () =>{
@@ -25,25 +26,31 @@ export const useSignup = () =>{
         setIsLoading(true)
     // 2. Firebase may raise signup Error
         try{
-    // 3. use Firebase Service to create a User
+    // 3. Create a User
             const res  = await projectAuth
                      .createUserWithEmailAndPassword(email, password)
-
     // 4. If Firebase does not respond
             if(!res){ throw new Error("Could not complete signup") }
 
-    // 6. DISPATCH LOGIN to set the authorized user on AuthContext
+    // 5. Set Current User on AuthContext
         dispatch({type: "SIGNUP", payload: res.user})
 
-    // ---> Upload Thumbnail
+    // 6. Upload Thumbnail to Firebase Storage
         const uploadPath = `thumbnails/${res.user?.uid}/${thumbnail.name}`
         const img = await projectStorage.ref(uploadPath).put(thumbnail);
         const imgURL = await img.ref.getDownloadURL();
 
-    // 5. update user Profile with display name & Image
+    // 7. update user Profile with display name & Image
             await res.user?.updateProfile({displayName, photoURL:imgURL})
 
-    // 7. If signup successful, reset state variables
+    // ---> Create Firestore Record for User - status, photoURL, Bio
+        await projectFirestore.collection("users")
+            .doc(res.user?.uid)
+            .set({
+                online:true, displayName, photoURL:imgURL
+            })
+
+    // 8. If signup successful, reset state variables
             if(!isCancelled) {
                 console.log("useSignup Not cancelled, updating State");
                 setError(null)
