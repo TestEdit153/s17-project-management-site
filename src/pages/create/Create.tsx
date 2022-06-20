effae4c21f5f74c8705bdb55ea5ff68cd2110302
dod <dod@mail.com>
2022-06-20 18:22:31 +0530

    Select component to Multiselect Registered Users

--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -1,23 +1,46 @@
 import styles from './Create.module.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import Select from 'react-select';
+import {useCollection} from "../../hooks/useCollection";
 
-type Users = [{
-  displayName:string
-  online:boolean
-  photoURL:string
-}] | []
+type User = { displayName:string, online:boolean, photoURL:string }
+type Users = Array<User> | []
+
+const categories = [
+    {value:'development', label: "Development"},
+    {value:'design', label: "Design"},
+    {value:'sales', label: "Sales"},
+    {value:'marketing', label: "Marketing"}
+]
 
 const Create = () =>{
     const [name, setName] = useState('');
     const [details, setDetails] = useState('');
     const [dueDate, setDueDate] = useState('');
-    const [category, setCategory] = useState('');
+    const [category, setCategory] = useState<any>('');
+
+
+    // 1. Users is Array<User>
+    const [users, setUsers] = useState<Users>([])
     const [assignedUsers, setAssignedUsers] = useState<Users>([]);
+
+    // 1. Get all users with useCollection Hook
+    const {error, documents} = useCollection('users')
+
+    // 2. useEffect maps users document for Select options
+    useEffect(() =>{
+        const options:any = documents?.map((user:User) =>{
+            return {value: user, label: user.displayName}
+        })
+        setUsers(options)
+    }, [documents])
+
 
     const handleSubmit = (e:any) =>{
         e.preventDefault();
-        console.table()
+        console.log({name, details, dueDate, category, assignedUsers})
     }
+
     return(
         <div className={styles.main}>
             <form onSubmit={handleSubmit}>
@@ -25,7 +48,7 @@ const Create = () =>{
                 <div className={styles["form-control"]}>
                     <label htmlFor="name" className="mb-4"> Name: </label>
                     <input type="text" id="name" required className={styles.name}
-                           value={name}
+                           value={name} placeholder={"Name for the Project"}
                            onChange={(e)=>{
                                setName(e.target.value)
                            }}
@@ -34,7 +57,7 @@ const Create = () =>{
                 <div className={styles["form-control"]}>
                     <label htmlFor="details" className="mb-4">Details: </label>
                     <textarea rows={3} id="details" required className={styles.details}
-                           value={details}
+                           value={details} placeholder={"Describe the Project here"}
                            onChange={(e)=>{
                                setDetails(e.target.value)
                            }}
@@ -52,26 +75,27 @@ const Create = () =>{
 
                 <div className="grid grid-cols-2 gap-2">
                     <div className={styles["form-control"]}>
-                        <label htmlFor="category" className="mb-4">Category: </label>
-                        <input type="text" id="category" required className={styles.category} value={category}
-                               onChange={(e)=>{
-                                   setCategory(e.target.value)
-                               }}
+                        <label htmlFor="category" className="mb-4"> Category: </label>
+                        <Select options={categories}
+                            onChange={(option) =>{
+                                    setCategory(option)
+                            }}
                         />
                     </div>
-                    <div className={styles["form-control"]}>
-                        <label htmlFor="assignedUsers" className="mb-4">Assigned Users: </label>
-                        <input type="text" id="assignedUsers" required className={styles.assignedUsers}
-                               onChange={(e)=>{
 
-                               }}
-                        />
-                    </div>
-                </div>
+        <div className={styles["form-control"]}>
+            <label htmlFor="asgUsers" className="mb-4"> Assigned Users: </label>
+            <Select options={users}
+                    onChange={(option) => {
+                        // @ts-ignore
+                        setAssignedUsers(option)
+                    }}
+                    isMulti
+            />
+        </div>
+    </div>
 
-                <div className={styles["btnSubmit"]}>
-                    <button>Submit</button>
-                </div>
+    <div className={styles["btnSubmit"]}> <button>Submit</button> </div>
             </form>
         </div>
     )
