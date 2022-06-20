ab97c3b0f66cbf0ffcaff94e2b52611f2d4c8d77
dod <dod@mail.com>
2022-06-20 18:53:56 +0530

    Add Custom validation for Form Errors

--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -2,23 +2,25 @@ import styles from './Create.module.css';
 import {useEffect, useState} from "react";
 import Select from 'react-select';
 import {useCollection} from "../../hooks/useCollection";
+import { alert_Error } from '../../helpers/AlertHelpers';
 
-type User = { displayName:string, online:boolean, photoURL:string }
+type User = { displayName: string, online: boolean, photoURL: string }
 type Users = Array<User> | []
 
 const categories = [
-    {value:'development', label: "Development"},
-    {value:'design', label: "Design"},
-    {value:'sales', label: "Sales"},
-    {value:'marketing', label: "Marketing"}
+    {value: 'development', label: "Development"},
+    {value: 'design', label: "Design"},
+    {value: 'sales', label: "Sales"},
+    {value: 'marketing', label: "Marketing"}
 ]
 
-const Create = () =>{
+const Create = () => {
     const [name, setName] = useState('');
     const [details, setDetails] = useState('');
     const [dueDate, setDueDate] = useState('');
     const [category, setCategory] = useState<any>('');
 
+    const [formError, setFormError] = useState<string | null>(null);
 
     // 1. Users is Array<User>
     const [users, setUsers] = useState<Users>([])
@@ -28,20 +30,30 @@ const Create = () =>{
     const {error, documents} = useCollection('users')
 
     // 2. useEffect maps users document for Select options
-    useEffect(() =>{
-        const options:any = documents?.map((user:User) =>{
+    useEffect(() => {
+        const options: any = documents?.map((user: User) => {
             return {value: user, label: user.displayName}
         })
         setUsers(options)
     }, [documents])
 
 
-    const handleSubmit = (e:any) =>{
+    const handleSubmit = (e: any) => {
         e.preventDefault();
+        setFormError(null);
+        if (!category) {
+            setFormError('Category cannot be Empty')
+            return
+        }
+        if (assignedUsers.length < 1) {
+            setFormError("Project should have atleast one user")
+            return
+        }
+
         console.log({name, details, dueDate, category, assignedUsers})
     }
 
-    return(
+    return (
         <div className={styles.main}>
             <form onSubmit={handleSubmit}>
                 <h3>Create a New Project</h3>
@@ -49,7 +61,7 @@ const Create = () =>{
                     <label htmlFor="name" className="mb-4"> Name: </label>
                     <input type="text" id="name" required className={styles.name}
                            value={name} placeholder={"Name for the Project"}
-                           onChange={(e)=>{
+                           onChange={(e) => {
                                setName(e.target.value)
                            }}
                     />
@@ -57,17 +69,17 @@ const Create = () =>{
                 <div className={styles["form-control"]}>
                     <label htmlFor="details" className="mb-4">Details: </label>
                     <textarea rows={3} id="details" required className={styles.details}
-                           value={details} placeholder={"Describe the Project here"}
-                           onChange={(e)=>{
-                               setDetails(e.target.value)
-                           }}
+                              value={details} placeholder={"Describe the Project here"}
+                              onChange={(e) => {
+                                  setDetails(e.target.value)
+                              }}
                     />
                 </div>
                 <div className={styles["form-control"]}>
                     <label htmlFor="dueDate" className="mb-4">Due Date: </label>
                     <input type="date" id="dueDate" required className={styles.dueDate}
                            value={dueDate}
-                           onChange={(e)=>{
+                           onChange={(e) => {
                                setDueDate(e.target.value)
                            }}
                     />
@@ -77,25 +89,31 @@ const Create = () =>{
                     <div className={styles["form-control"]}>
                         <label htmlFor="category" className="mb-4"> Category: </label>
                         <Select options={categories}
-                            onChange={(option) =>{
+                                onChange={(option) => {
                                     setCategory(option)
-                            }}
+                                }}
                         />
                     </div>
 
-        <div className={styles["form-control"]}>
-            <label htmlFor="asgUsers" className="mb-4"> Assigned Users: </label>
-            <Select options={users}
-                    onChange={(option) => {
-                        // @ts-ignore
-                        setAssignedUsers(option)
-                    }}
-                    isMulti
-            />
-        </div>
-    </div>
+                    <div className={styles["form-control"]}>
+                        <label htmlFor="asgUsers" className="mb-4">
+                                Assigned Users:
+                        </label>
+                        <Select options={users}
+                                onChange={(option) => {
+                                    // @ts-ignore
+                                    setAssignedUsers(option)
+                                }}
+                                isMulti
+                        />
+                    </div>
+                </div>
 
-    <div className={styles["btnSubmit"]}> <button>Submit</button> </div>
+                {alert_Error(formError)}
+                {alert_Error(error)}
+                <div className={styles["btnSubmit"]}>
+                    <button>Submit</button>
+                </div>
             </form>
         </div>
     )
